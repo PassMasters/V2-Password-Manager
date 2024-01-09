f40f4d7f6942b2006f131f17d3dae757703896d7
cs947939 <sufam5@outlook.com>
2024-01-09 16:53:16 +0000

    oops broke security

--- security/urls.py
+++ security/urls.py
@@ -1,10 +1,1 @@
-from os import name
-from django.urls import path,include
-from . import views
-from django.conf.urls.static import static
-from django.views.generic import TemplateView
-urlpatterns = [
-    path('destroyaccount', views.deleteAccount),
-    path('add',  views.add),
-    path('setup', views.setup)
-]
+urlpatterns = []
