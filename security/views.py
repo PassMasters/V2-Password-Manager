fde5a5d586d7afd9b59a48de5542fb8a85adb4bb
cs947939 <sufam5@outlook.com>
2024-01-10 17:53:20 +0000

    khhj

--- security/views.py
+++ security/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 def logon(request):
     if request.method != "POST":
         return render(request, "registration/login.html")
