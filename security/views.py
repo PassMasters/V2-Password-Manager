8d0cb8a3fcfaf9c183973b94ece7168f998fbf23
cs947939 <sufam5@outlook.com>
2024-01-12 20:19:35 +0000

    password reset

--- security/views.py
+++ security/views.py
@@ -5,32 +5,43 @@ from django.contrib.auth.models import User
 from pwmanager.models import Encryption
 from .models import PWcheck
 import bcrypt
+from . import crypto
 from Crypto.Cipher import AES
 def passwordreset(request):
     if request.method != "POST":
         return render(request, "pwreset.html")
     else:
         user = request.POST.get('Username')
-        user2 = User.objects.get(Username=user)
+        user2 = User.objects.get(username=user)
         ekey = Encryption.objects.get(Owner=user2)
         pwcheck = PWcheck.objects.get(Owner=user2)
         pin = bytes(request.POST.get('pin'), 'UTF-8')
-        salt = bytes(ekey.Salt, 'UTF-8')
+        salt = eval(bytes(ekey.Salt, 'UTF-8'))
         iv = eval(bytes(ekey.IV, 'UTF-8'))
+        print(salt)
+        print(iv)
         encryption_key = bcrypt.kdf(pin, salt, rounds=500,  desired_key_bytes=32)
+        print(encryption_key)
         keys = AES.new(encryption_key, AES.MODE_CBC, iv)
         answer = pwcheck.Answer
-        data = eval(pwcheck.Data)
-        datade = keys.decrypt(data)
-        padding_length = datade[-1]
-        plaintext_bytes = datade[:-padding_length]
-        datade = str(plaintext_bytes, 'UTF-8')
+        data = eval(bytes(pwcheck.Data, 'UTF-8'))
+        print(data)
+        print(data)
+        try:
+            datade = crypto.decrypt(data, keys)
+            if datade == '':
+                datade = keys.decrypt(data)
+                padding_length = datade[-1]
+                plaintext_bytes = datade[:-padding_length]
+                datade = str(plaintext_bytes, 'UTF-8')
+        except Exception:
+            return render(request, "dead.html")
         if datade != answer:
             return render(request, "pwreset.html", {'msg': "wrong pin"})
         else:
             newpw = request.POST.get('newpw')
             user2.set_password(newpw)
-    return render(request, 'complete.html')
+            return render(request, 'complete.html')
 
 
 def logon(request):
